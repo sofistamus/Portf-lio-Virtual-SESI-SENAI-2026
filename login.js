0fa26840bb9b1e697089287be484ebc585779957
Sofia <sofia.b.silva7@aluno.senai.br>
2026-09-17 17:00:42 -0300

    Portfólio

--- login.js
+++ login.js
@@ -2,7 +2,11 @@ function fazerLogin() {
     const usuario = document.getElementById("usuario").value;
     const senha = document.getElementById("senha").value;
 
-    if (usuario === "sofia" && senha === "123456") {
+    if (
+        (usuario === "admin" && senha === "1234") ||
+        (usuario === "prof" && senha === "321") ||
+        (usuario === "sofia" && senha === "375")
+    ) {
         window.location.href = "folio.html";
     } else {
         alert("Usuário ou senha incorretos!");
